75cfb7a020eaaf5007a55df14aca4e45c6ba03c0
Espen Trydal <espen@fastmail.com>
2017-10-05 13:09:59 +0200

    virker fortsatt ikke, getchar gir bare LF

--- celsiusconv.c
+++ celsiusconv.c
@@ -1,37 +1,25 @@
-// lab1-9.c
+// celsiusconv.c
 // Enkel kalkulator for å konvertere C til F eller K.
 // Forsøke å få getchar() til å fungere.
 #include <stdio.h>
-#include <unistd.h>
 
 int main()
 {
-    int celsius = 0;
     printf("Tast inn temperatur i grader Celsius: ");
+    int celsius = 0;
     scanf("%d", &celsius);
     printf("Skal denne temperaturen konverteres til F eller K?\n");
-//    int max = 100;
- //   char* valg = (char*)malloc(max);
-    char c;
-    while(c!=49 | c!=50) {
-        printf("Trykk 1 for F eller 2 for K: ");
-        if(c!=EOF)
-            c = getchar();
-            if(c==49 | c==50)
-                break;
+    int valg = getchar();
+    while (valg!='F' || valg!='f' || valg!='K' || valg!='k')
+        printf("%d\t%c", valg, valg);
+
+    if (valg=='F' || valg=='f') {
+        int fahren = 32 + celsius*(9/5);
+        printf("\n%d C tilsvarer %d F.\n", celsius, fahren);
     }
-
-    int kelvin = 0;
-    int fahrenheit = 0;
-    kelvin = 273+celsius;
-    fahrenheit = 32 + celsius*(9/5);
-
-    if (c=='1')
-        printf("\n%d C tilsvarer %d F.\n", celsius, fahrenheit);
-    if (c=='2')
+    if (valg=='K' || valg=='k') {
+        int kelvin = 273+celsius;
         printf("\n%d C tilsvarer %d K.\n", celsius, kelvin);
-
-    printf("Trykk Ctrl-C for å fortsette...\n");
-    pause();
+    }
     return 0;
 }
