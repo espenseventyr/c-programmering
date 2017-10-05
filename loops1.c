c6c9dbe248da369dce8f33251e79414afc931878
Espen Trydal <espen@fastmail.com>
2017-10-05 12:47:03 +0200

    formatting

--- loops1.c
+++ loops1.c
@@ -1,46 +1,46 @@
 // loops1.c
 // Program for å lese inn alder på 10 studenter og så regne
 // ut gjennomsnittsalderen.
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
     printf("Skriv inn alder på 10 studenter.\n");
     printf("Trykk enter mellom hver gang.\n");
     int alder[10];
-    for(int i = 0; i<10; i++)
+    for (int i = 0; i<10; i++)
         scanf("%d", &alder[i]);
     printf("Alderen på studentene du skrev inn var:\n");
-    for(int i = 0; i<10; i++)
+    for (int i = 0; i<10; i++)
         printf("%d: %d\n", i+1, alder[i]);
     int sum_alder = 0;
-    for(int i = 0; i<10; i++)
+    for (int i = 0; i<10; i++)
         sum_alder += alder[i];
     double snittalder = sum_alder/10.0;
     printf("\nGjennomsnittsalderen er: %.2f\n", snittalder);
 
 
     char valg = 0;
-    while(1) {
+    while (1) {
         printf("Ønsker du å korrigere alder på en student?\n");
         printf("Velg studentnummer eller skriv 'n' for å avslutte.\n");
         getchar(); // Be om forklaring på dette, skjønner ikke hvorfor det er nødvendig.
         scanf("%c", &valg);
-        if(valg==78 || valg==110)
+        if (valg=='n' || valg=='N')
             break;
-        if(valg>48 && valg<58) {
+        if (valg>'1' && valg<'9') {
             printf("Skriv inn ny alder: ");
             scanf("%d", &alder[(int)valg-49]);
             printf("Alderen på studentene du skrev inn var:\n");
-            for(int i = 0; i<10; i++) {
-                if(i==(int)valg-49) {
+            for (int i = 0; i<10; i++) {
+                if (i==(int)valg-49) {
                     printf("%d: %d (korrigert)\n", i+1, alder[i]);
                     continue;
                 }
                 printf("%d: %d\n", i+1, alder[i]);
             }
             sum_alder = 0;
-            for(int i = 0; i<10; i++)
+            for (int i = 0; i<10; i++)
                 sum_alder += alder[i];
             snittalder = sum_alder/10.0;
             printf("\nGjennomsnittsalderen er: %.2f\n", snittalder);
